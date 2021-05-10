f643dbcd0f1170a8b63d8857eda92554da5be08d
deborahtrez <deborahtrez12@gmail.com>
2021-05-11 01:50:46 +0300

    Updates

---
+++ precache-manifest.d7d22dea4518e870358a2031beaaf3fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0939cc926e642f026352f73715fe09ff",
+    "url": "/MIGRNT-React-dashboard/index.html"
+  },
+  {
+    "revision": "d48f5d36371b12488eea",
+    "url": "/MIGRNT-React-dashboard/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8fc23af59b14cf1a990b",
+    "url": "/MIGRNT-React-dashboard/static/css/main.e5412b90.chunk.css"
+  },
+  {
+    "revision": "d48f5d36371b12488eea",
+    "url": "/MIGRNT-React-dashboard/static/js/2.d95fe791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MIGRNT-React-dashboard/static/js/2.d95fe791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc23af59b14cf1a990b",
+    "url": "/MIGRNT-React-dashboard/static/js/main.278f1e46.chunk.js"
+  },
+  {
+    "revision": "0be43151edb549a077da",
+    "url": "/MIGRNT-React-dashboard/static/js/runtime-main.66daeb4b.js"
+  }
+]);
